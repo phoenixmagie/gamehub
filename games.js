942d35ba65d5b4e3baf96e642d11d158f4a093fb
phoenixmagie <phoenixmagie.mc@gmail.com>
2026-04-15 20:14:21 +0200

    Aktualisieren von games.js

--- games.js
+++ games.js
@@ -7,14 +7,6 @@ const GAMES_DATA = [
     icon: "pictures/icons/cookie.png",
     btn_color: "#D2691E"
 },
-    {
-        id: "plane-catapult",
-        game_name: "Plane Catapult",
-        path: "games/plane-catapult/index.html",
-        icon: "pictures/icons/plane.png",
-        btn_color: "#3498db" // Blau für das Flugzeug-Spiel
-    },
-
     /*{
         id: "beispiel",
         game_name: "Beispiel",
