3054eb90e51e9d34cb05a8cf86c566cf955c7b9f
phoenixmagie <phoenixmagie.mc@gmail.com>
2026-04-15 13:53:52 +0200

    games.js erstellen

---
+++ games.js
@@ -0,0 +1,25 @@
+// games.js
+const GAMES_DATA = [
+    {
+        id: "snake_01",
+        game_name: "Snake Classic",
+        path: "games/snake/index.html",
+        icon: "pictures/icons/snake.png",
+        btn_color: "#ff4444"
+    },
+    {
+        id: "cookie_clicker",
+        game_name: "Cookie Clicker",
+        path: "games/cookie-clicker/index.html",
+        icon: "pictures/icons/cookie.png",
+        btn_color: "#b30000"
+    },
+    {
+        id: "geo_dash",
+        game_name: "Geometry Dash Klon",
+        path: "games/geometry-dash/index.html",
+        icon: "pictures/icons/geodash.png",
+        btn_color: "#e60000"
+    }
+    // Hier kannst du bis zu 100 Spiele ergänzen...
+];
