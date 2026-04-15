194eefcd3f4c50d6ed66e5854b170ed10235a749
phoenixmagie <phoenixmagie.mc@gmail.com>
2026-04-15 20:16:08 +0200

    Aktualisieren von games.js

--- games.js
+++ games.js
@@ -7,6 +7,13 @@ const GAMES_DATA = [
     icon: "pictures/icons/cookie.png",
     btn_color: "#D2691E"
 },
+{
+    id: "tetris",
+    game_name: "Tetris",
+    path: "games/tetris/index.html",
+    icon: "pictures/icons/tetris.png", // Bildpfad anpassen
+    btn_color: "#9b59b6" // Lila
+},
     /*{
         id: "beispiel",
         game_name: "Beispiel",
